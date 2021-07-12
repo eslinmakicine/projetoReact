864f99687e65e97be911430fe8be084bc58d606a
eslinmakicine <eslinmartins@gmail.com>
2021-07-12 20:13:34 -0300

    Aula 11 - Tratamento de erros e use History

--- src/Pages/Repositories/index.js
+++ src/Pages/Repositories/index.js
@@ -1,15 +1,23 @@
 import * as S from './styled'; //dando variavel e importando o styled.js
 import React, {useEffect, useState} from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Repositories() {
     const [repositories, setRepositories] = useState([]); //define o estado. Pegando o valor do estado, e a função que vai setar o valor do estado. Inicialmente será um array vazio
+    const history = useHistory();
 
     useEffect(() => {
         let repositoriesName = localStorage.getItem('repositoriesName'); //ele vai armazenas em repositoriesName o valor que está no localStorage
-        repositoriesName = JSON.parse(repositoriesName); //transforma num objeto
-        console.log(repositoriesName);
-        setRepositories(repositoriesName); //vai setar o setRepositories passando o repositoriesName
- //      localStorage.clear();
+        if (repositoriesName !== null) {
+            repositoriesName = JSON.parse(repositoriesName); //transforma num objeto
+            console.log(repositoriesName);
+            setRepositories(repositoriesName); //vai setar o setRepositories passando o repositoriesName
+            localStorage.clear();
+        } else {
+            history.push('/'); //ao pesquisar o repositorio, é enviado para a page /home
+        }
+        
+ 
     }, []); //se nao passar nenhum argumento em [], ele vai monitorar quando o componente for montado(renderizado)
 
     return (
